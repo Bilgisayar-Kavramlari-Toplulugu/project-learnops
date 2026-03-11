7d3b22e4937496d1885ad3c0964cc247129141f7
Alper <alper.makcan@gmail.com>
2026-03-11 16:02:36 +0300

    refactor: move auth-provider under providers folder (#19)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { QueryProvider } from "@/providers/query-provider";
 import "./globals.css";
-import AuthProvider from "@/components/layout/auth-provider";
+import AuthProvider from "@/providers/auth-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
