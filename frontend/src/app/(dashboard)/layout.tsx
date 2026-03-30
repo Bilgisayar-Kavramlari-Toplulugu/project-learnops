1b61be90c0ca61ed273c85c34a6e9907020c587c
Alper <alper.makcan@gmail.com>
2026-03-30 23:08:22 +0300

    feat(frontend): add sekeleton component for dashboardshell (#FE-05-06-07)

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -6,12 +6,13 @@ import { usePathname } from "next/navigation";
 import { DashboardShell } from "@/components/layout/dashboard-shell";
 import { dashboardSidebarItems } from "@/lib/dashboard-ui.config";
 import { useProfile } from "@/hooks/profile/use-profile";
+import { DashboardShellSkeleton } from "@/components/ui/dashboardshell-skeleton";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const { data: profile, isLoading } = useProfile();
 
-  if (isLoading || !profile) return null;
+  if (isLoading || !profile) return <DashboardShellSkeleton />;
 
   return (
     <DashboardShell
