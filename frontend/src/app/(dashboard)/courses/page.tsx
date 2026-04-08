e617f59598767a43d4b199b0eee0652a8bd79571
Alper <alper.makcan@gmail.com>
2026-04-08 20:56:02 +0300

    refactor(frontend): update courses page return value (#FE-08)
    The backend endpoint for fetching courses is not implemented yet, because of that I'm using fake-courses data that I created for development purposes only.
    Courses page returns CourseClient component now on.

--- frontend/src/app/(dashboard)/courses/page.tsx
+++ frontend/src/app/(dashboard)/courses/page.tsx
@@ -1,28 +1,19 @@
+import CoursesClient from "@/components/features/courses/courses-client";
 import WrapperContainer from "@/components/features/dashboard/wrapper-container";
-import { CourseListClient } from "@/components/features/courses/course-list-client";
-import { getCourses } from "@/lib/courses";
+import course_list from "@/data/fake-courses";
 
-export const metadata = {
-  title: "Kurslar | LearnOps",
-  description: "Kendinizi geliştirmeniz için hazırlanan profesyonel eğitim içerikleri.",
-};
+/* async function getCourses() {
+  return fetch("https://api.example.com/courses", {
+    cache: "force-cache",
+  }).then((res) => res.json());
+} */
 
 export default async function CoursesPage() {
-  const [courses] = await Promise.all([
-    getCourses(),       // DB veya API fetch
-  ]);
+  // const courses = await getCourses();
+
   return (
     <WrapperContainer>
-      <div className="mb-10 space-y-2">
-        <h1 className="text-3xl font-bold tracking-tight text-slate-900 dark:text-slate-100 sm:text-4xl">
-          Eğitim Kataloğu
-        </h1>
-        <p className="text-lg text-slate-500 dark:text-slate-400">
-          Yeni yetenekler edinin ve kariyerinizde bir adım öne geçin.
-        </p>
-      </div>
-
-      <CourseListClient initialCourses={courses} />
+      <CoursesClient courses={course_list} />
     </WrapperContainer>
   );
 }
